5a837c58a8d974defd99be48fc9b936d332323fe
PietroNardini <pietro01.nardini@edu.unife.it>
2025-02-24 16:48:16 +0100

    Aggiunto un ciclo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Count: " + i);
+        }
     }
 }
